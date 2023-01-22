dd4917dadfbd48d8268e9d45883305d7911e26e5
FELIPE ARRUDA DE SOUZA <felipe_rhnossa@3coracoes.com.br>
2023-01-21 21:25:52 -0300

    Sexto commit

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,21 +1,28 @@
 import { Photo } from "../types/Photo";
 import { storage } from "../libs/firebase"; //Uma variável que tem referência ao meu storage
-import { ref, listAll, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  listAll,
+  getDownloadURL,
+  updateMetadata,
+  uploadBytes,
+} from "firebase/storage";
+import { v4 as createId } from "uuid"; //Essa lib possui vários recursos, um deles é o v4, que gera um hash aleatório.
 
 export const getAll = async () => {
   let list: Photo[] = [];
   //Primeiro eu faço referência ao meu storage, e depois eu faço uma referência a pasta que eu quero acessar.
-  let imagesFolder = ref(storage, "images")
+  let imagesFolder = ref(storage, "images");
   // A função listAll precisa de uma referência(Ela vai listar tudo que tiver lá), e ela vai me retornar uma promisse, e no final um ListResult
-  let photoList = await listAll(imagesFolder)
+  let photoList = await listAll(imagesFolder);
 
   for (let i in photoList.items) {
-    let photoUrl = await getDownloadURL(photoList.items[i])
+    let photoUrl = await getDownloadURL(photoList.items[i]);
     //Preciso gerar a minha url, pois não tenho por padrão!
     list.push({
       name: photoList.items[i].name,
-      url: photoUrl
-    })
+      url: photoUrl,
+    });
   }
   return list;
 };
@@ -28,3 +35,22 @@ ANOTAÇÕES:
 
 -Então, primeiro, eu tenho que fazer referência ao que eu vou fazer, e só depois, eu faço algo.
 */
+
+export const insertFile = async (file: File) => {
+  if (["image/jpeg", "image/jpg", "image/png"].includes(file.type)) {
+    let randomName = createId();
+    let newFile = ref(storage, `images/${randomName}`);
+
+    let upload = await uploadBytes(newFile, file);
+
+    let photoUrl = await getDownloadURL(upload.ref); //Estou enviando a referêncnia do arquivo utilizando o ref, e com isso, gero a url e dou um get.
+
+    //o Ref me retorna a referência desse arquivo em específico.
+    return {
+      name: upload.ref.name,
+      url: photoUrl,
+    };
+  } else {
+    return new Error("Tipo de arquivo não permitido!");
+  }
+};
